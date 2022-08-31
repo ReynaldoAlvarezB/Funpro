f2925f862ec521f9b0b92187d536d0754caad2a8
ReynaldoAlvarezB <alvarezreynaldo2104@gmail.com>
2022-08-30 19:49:19 -0500

    Semana 2. Corrección de ejercicios 1

--- Semana 2/1.c
+++ Semana 2/1.c
@@ -1,29 +1,38 @@
 #include<stdio.h>
 #include<math.h>
 
+
+// Iniciar el archivo de C
+// Declarar dos variables:
+    //El lado ( lo que vamos a recibir)
+    //El cuadrado, lo que vamos a calcular
+
+//Pedir el dato del lado
+//Calcular el área multiplicando lado x lado
+//Mostrar el área
+
 /*
 1. ´Area del cuadrado
 Dado el lado de un cuadrado, se pide calcular el ´area del mismo. Recordar que el ´area del cuadrado
 es igual al lado2.
-
-
-
 */
+
 
 int main() {
 
-    int lado, area;
+    double lado, area;
 
     printf("Ingrese el lado del cuadrado: ");
 
-    scanf("%i", &lado);
-
+    scanf("%lf", &lado);
+    //Importar math.h
     area = pow(lado,2);
+
     
-    printf("El area es %i\n", area);
+    printf("El area es %lf \n", area);
 
 
 		
-	main(); //Solo pongo esto para que se vuelva a ejecutar al terminar
+	main(); //Solo pongo esto para que se vuelva a ejecutar al terminar y probar más fácilmente todos los casos de la guía, tú no lo hagas.
 	return 0;
 }
